e26a605978425134a20f73b74e399c6437300e54
Indrajit Roy <70997867+indrajit-roy@users.noreply.github.com>
2022-03-30 13:31:16 +0530

    first commit - verification success

---
+++ app/src/main/java/com/example/applinkstest/AppLinksActivity.kt
@@ -0,0 +1,11 @@
+package com.example.applinkstest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppLinksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_links)
+    }
+}
